672c9fadb22f6027b6b98ab14080c89aa89b238a
LeoKim <leo3179@naver.com>
2022-07-17 19:13:02 +0900

    FIX / 레이드 모듈, RaidConsumer import 에러 수정
    - 배포 과정에서 레이드 모듈에서 임포트가 되지 않는 이슈를 발견해 수정했습니다
    - 스웨거는 일관성만 가지게 손봤습니다
    - 바로 메인으로 pr하겠습니다
    
    Resolve: [Task] 스웨거 리스폰스 타입 지정, example, description #89

--- src/utils/responseHandler/response.enum.ts
+++ src/utils/responseHandler/response.enum.ts
@@ -6,9 +6,10 @@ export const MSG = {
   logoutUser: { code: 200, msg: '유저 로그아웃 요청에 성공했습니다!' },
   verifyUser: { code: 200, msg: '유저 인증 성공에 성공했습니다!' },
   refreshTokenWithUser: { code: 200, msg: '유저 인증 성공에 성공했습니다!' },
-  
+
   // Boss Raid
   enterBossRaid: { code: 201, msg: '보스 레이드에 입장했습니다!' },
   getRaidStatus: { code: 200, msg: '보스 레이드 상태 정보 조회에 성공했습니다!' },
-  endBossRaid: { code: 200, msg: '보스레이드를 정상적으로 종료했습니다.'},
+  endBossRaid: { code: 200, msg: '보스레이드를 정상적으로 종료했습니다!' },
+  getRank: { code: 201, msg: '보스 레이드 랭킹 조회에 성공했습니다!' },
 };
